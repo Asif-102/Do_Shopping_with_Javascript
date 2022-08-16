0f4a5908aa4d546eb1948670134545e559134658
Asif-102 <asif1zero2@gmail.com>
2022-08-17 00:57:05 +0600

    diplay cart products and calculate total price

--- js/app.js
+++ js/app.js
@@ -1,5 +1,36 @@
 const cart = [];
+// products-cart
 
+function displayProduct()
+{
+    let totalPrice = 0;
+    const totalProducts = document.getElementById("total-products");
+    totalProducts.innerText = cart.length;
+
+    const cartContainer = document.getElementById("products-cart");
+    cartContainer.textContent = '';
+
+    for(let i = 0; i < cart.length; i++)
+    {
+        totalPrice += cart[i].pdPrice;
+
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <th>${i+1}</th>
+        <td>${cart[i].pdName}</td>
+        <td>${cart[i].pdPrice}</td>
+        `;
+        cartContainer.appendChild(tr);
+    }
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <th></th>
+    <td>Total Price</td>
+    <td>${totalPrice}</td>
+    `;
+
+    cartContainer.appendChild(tr);
+}
 
 function addToCart(element) {
     const pdName = element.parentNode.parentNode.children[0].innerText;
@@ -7,8 +38,10 @@ function addToCart(element) {
 
     const pd = {
         pdName: pdName,
-        pdPrice: pdPrice
+        pdPrice: parseInt(pdPrice)
     }
 
     cart.push(pd);
+    
+    displayProduct();
 }
